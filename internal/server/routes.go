7d6e4a9c301052eb57f0263a650aacb97524bdd6
sumitst05 <sumitst05@gmail.com>
2024-05-04 20:54:57 +0530

    update

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -16,16 +16,16 @@ func (s *Server) RegisterRoutes() http.Handler {
 
 	router.HandleFunc("/", handler.HomeHandler())
 
-	router.HandleFunc("/cards", handler.CardHandler())
+	router.HandleFunc("GET /cards", handler.CardHandler())
 
-	router.HandleFunc("/shrink/png", handler.HandlePNG())
-	router.HandleFunc("/shrink/jpg", handler.HandleJPG())
-	router.HandleFunc("/shrink/pdf", handler.HandlePDF())
-	router.HandleFunc("/shrink/docx", handler.HandleDOCX())
-	router.HandleFunc("/shrink/mp3", handler.HandleMP3())
-	router.HandleFunc("/shrink/wav", handler.HandleWAV())
-	router.HandleFunc("/shrink/mp4", handler.HandleMP4())
-	router.HandleFunc("/shrink/mkv", handler.HandleMKV())
+	router.HandleFunc("POST /shrink/png", handler.HandlePNG())
+	router.HandleFunc("POST /shrink/jpg", handler.HandleJPG())
+	router.HandleFunc("POST /shrink/pdf", handler.HandlePDF())
+	router.HandleFunc("POST /shrink/docx", handler.HandleDOCX())
+	router.HandleFunc("POST /shrink/mp3", handler.HandleMP3())
+	router.HandleFunc("POST /shrink/wav", handler.HandleWAV())
+	router.HandleFunc("POST /shrink/mp4", handler.HandleMP4())
+	router.HandleFunc("POST /shrink/mkv", handler.HandleMKV())
 
 	return router
 }
